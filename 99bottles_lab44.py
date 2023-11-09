92f497e6e28084b4aed4286efb215a46b05d5972
s4gr4nt <stephaniearrieta0412@gmail.com>
2023-11-09 22:36:35 +0000

    Playing with python pip packages

---
+++ 99bottles_lab44.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import time
+
+
+while True:  
+    num = int(input("How many bottles of beer should we pass around? "))
+    if num == 0:
+        print("Goodbye.")
+        break
+    elif num > 100:
+        print("Choose a number equal to or less than 100!")
+
+    elif num <= 0:
+        print("Choose a number higher than 1!")
+
+    else:
+        for i in range(num, 0, -1):
+            print(f"{i} bottles of beer on the wall!")
+            time.sleep(1)
+            print(f"{i} bottles of beer on the wall! {i} bottles of beer! You take one down, pass it around!")
+            time.sleep(1)
+
+
